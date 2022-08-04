92fa9e07bef22fa0f5d09df1c84cf84a1a09c73f
Caio-Duarte-EM <caio.duarte@escolarmanager.com.br>
2022-08-04 13:58:58 -0300

    Adicionando arquivos do projeto

---
+++ Validador de CPF  e CNPJ/Validador de CPF  e CNPJ/Cpf.cs
@@ -0,0 +1,80 @@
+﻿namespace Validador_de_CPF_e_CNPJ;
+public readonly struct Cpf
+{
+    public readonly string Valor;
+
+    private Cpf(string valorValido)
+    {
+
+        Valor = ValorCpfEhValido(valorValido) ? FormateCpf(valorValido) : "000.000.000-00";
+    }
+
+    public static implicit operator string(Cpf cpf) => cpf.Valor;
+    public static explicit operator Cpf(string v) => new(v);
+
+    static bool ValorCpfEhValido(string cpf)
+    {
+        if (string.IsNullOrWhiteSpace(cpf))
+        {
+            return false;
+        }
+
+        if (cpf.Contains(' '))
+        {
+            return false;
+        }
+
+        if (cpf.Length != 11)
+        {
+            return false;
+        }
+
+        bool cpfEhNumerico = long.TryParse(cpf, out _);
+        if (cpfEhNumerico == false)
+        {
+            return false;
+        }
+
+        int somaDigitos = 0;
+        int multiplicacaoDigitos;
+        for (int i = 0; i < 9; i++)
+        {
+            multiplicacaoDigitos = int.Parse(cpf[i].ToString()) * (i + 1);
+            somaDigitos += multiplicacaoDigitos;
+        }
+
+        int primeiroDigitoVerificador = somaDigitos % 11;
+        if (primeiroDigitoVerificador == 10)
+        {
+            primeiroDigitoVerificador = 0;
+        }
+        if (primeiroDigitoVerificador != int.Parse(cpf[9].ToString()))
+        {
+            return false;
+        }
+
+        somaDigitos = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            multiplicacaoDigitos = int.Parse(cpf[i].ToString()) * (i);
+            somaDigitos += multiplicacaoDigitos;
+        }
+
+        int segundoDigitoVerificador = somaDigitos % 11;
+        if (segundoDigitoVerificador == 10)
+        {
+            segundoDigitoVerificador = 0;
+        }
+        if (segundoDigitoVerificador != int.Parse(cpf[10].ToString()))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    static string FormateCpf(string cpf)
+    {
+        return Convert.ToUInt64(cpf).ToString(@"000\.000\.000\-00");
+    }
+}
---
+++ Validador de CPF  e CNPJ/Validador de CPF  e CNPJ/Program.cs
@@ -0,0 +1,34 @@
+﻿using Validador_de_CPF_e_CNPJ;
+
+Console.WriteLine("O que deseja validar? \n" +
+    "(1) - CPF\n" +
+    "(2) - CNPJ");
+string? opcao = Console.ReadLine()?.Trim();
+if (opcao == "1")
+{
+    Console.Write("Digite um CPF: ");
+    string valor = Console.ReadLine()?.Trim().Replace(".", "").Replace("-", "") ?? "";
+    Cpf cpf = (Cpf)valor;
+    if(cpf == "000.000.000-00")
+    {
+        Console.Write("CPF inválido\n" +
+            "Valor Padrão: ");
+    }
+    Console.WriteLine(cpf.Valor);
+}
+else if(opcao == "2")
+{
+    Console.Write("Digite um CNPJ: ");
+    string valor = Console.ReadLine()?.Trim().Replace(".", "").Replace("-", "").Replace("/", "") ?? "";
+    Cnpj cnpj = (Cnpj)valor;
+    if (cnpj == "00.000.000/0001-00")
+    {
+        Console.Write("CNPJ inválido\n" +
+            "Valor Padrão: ");
+    }
+    Console.WriteLine(cnpj.Valor);
+}
+else
+{
+    Console.WriteLine("Opção inválida");
+}
